e19290f5a430e44206165f1741213534024fe5b8
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-08 15:19:11 -0700

    can assign elevators to different floors

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,16 @@
 class Building < ActiveRecord::Base
   has_many :elevators
+
+g  def assign_elevators_to_floors
+    num_floors = self.floors
+    num_elevators = self.elevators.count
+
+    num_sections = num_elevators
+    floors_per_section = num_floors / num_sections
+
+    elevators.each_with_index do |elevator, index|
+      new_floor = (index * floors_per_section) + (floors_per_section / 2)
+      elevator.move_to(new_floor)
+    end
+  end
 end
