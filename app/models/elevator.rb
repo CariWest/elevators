3b5577d4e393df686116ee74f9b3a1468ca608eb
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-08 15:21:32 -0700

    add methods to calculate figure of suitability based on direction & floor

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -6,4 +6,23 @@ class Elevator < ActiveRecord::Base
     self.save!
   end
 
+  def stationary
+    @building_floors - distance
+  end
+
+  def moving_same_direction
+    (@building_floors + 2) - distance
+  end
+
+  def moving_opp_direction
+    (@building_floors + 1) - distance
+  end
+
+  def moving_away
+    1
+  end
+
+  def distance
+    (self.floor - @floor_called).abs
+  end
 end
