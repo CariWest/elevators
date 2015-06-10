a47d35a6d226fab6aada5d096381f91f4e0d77a6
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-10 13:52:43 -0700

    find the elevator

--- app/controllers/elevators_controller.rb
+++ app/controllers/elevators_controller.rb
@@ -3,9 +3,15 @@ class ElevatorsController < ApplicationController
   def nearest
     # expects params: { floor: [current_floor], direction: [direction]}, where floor is an integer and direction is either "up" or "down"
 
-    Elevator.nearest({
-      floor: params[:floor],
-      direction: params[:direction]
+    # stubbed for complexity
+    @building = Building.first
+
+    floor_called = params[:floor]
+    direction = params[:direction]
+
+    elevator = @building.find_elevator({
+      floor: floor,
+      direction: direction
     })
   end
 
