ad7c3fa6856282d30ccca4c03c483a95f278a706
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-09 20:50:15 -0700

    consider what the api endroute will look like & what parameters it will pass

--- app/controllers/elevators_controller.rb
+++ app/controllers/elevators_controller.rb
@@ -1,2 +1,12 @@
 class ElevatorsController < ApplicationController
+
+  def nearest
+    # expects params: { floor: [current_floor], direction: [direction]}, where floor is an integer and direction is either "up" or "down"
+
+    Elevator.nearest({
+      floor: params[:floor],
+      direction: params[:direction]
+    })
+  end
+
 end
