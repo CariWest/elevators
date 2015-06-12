dbfa78abf46e09663a8ec1798e9eeb197e960e59
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-11 20:58:15 -0700

    add methods to construct json objects

--- app/controllers/elevators_controller.rb
+++ app/controllers/elevators_controller.rb
@@ -10,18 +10,33 @@ class ElevatorsController < ApplicationController
     direction = params[:direction]
 
     elevator = building.call_elevator(floor_called, direction)
+
+    render json: construct_elevator_json_object(elevator, floor_called, direction)
+
+  end
+
+  def construct_elevator_json_object(elevator, floor_called, direction)
     figure_of_suitability = elevator.figure_of_suitability(floor_called, direction)
 
-    render json: {
-      floor_called:           floor_called,
-      direction_called:       direction,
-      elevator:               elevator,
-      elevator_floor:         elevator.floor.floor_num,
-      elevator_direction:     elevator.direction,
-      figure_of_suitability:  figure_of_suitability
+    {
+      floor_called:     floor_called.floor_num,
+      direction_called: direction,
+      elevator: {
+        id:                     elevator.id,
+        origin_floor:           elevator.floor.floor_num,
+        moving_direction:       elevator.direction,
+        figure_of_suitability:  figure_of_suitability,
+        queued_floors:          elevator.queued_floors.sort_by { |floor| floor.floor_num }.map { |floor| construct_floor_json_object(floor) }
+      }
     }
   end
 
+  def construct_floor_json_object(floor)
+    {
+      id:                 floor.id,
+      floor_num:          floor.floor_num,
+      waiting_passengers: floor.waiting_passengers
+    }
   end
 
 end
