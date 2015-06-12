c7c2ae2abfd836c988bd164fa8d4f6b01d6f2b37
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-11 20:56:08 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+building = Building.create
+
+10.times do |x|
+  building.floors.create(building: building, floor_num: x + 1, waiting_passengers: true)
+end
+
+2.times do
+  building.elevators.create(building: building, direction: "stationary")
+end
+
+building.assign_elevators_to_floors
